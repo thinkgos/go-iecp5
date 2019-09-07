ea0df8751b4b06043cd243de3f514e511cf17448
mo <thinkgo@aliyun.com>
2019-09-07 23:33:28 +0800

    在监视方向系统信息的应用服务数据单元 注释

--- asdu/msys.go
+++ asdu/msys.go
@@ -1,9 +1,13 @@
 package asdu
 
-// EndOfInitialization send a type identification M_EI_NA_1, subclass 7.3.3.1
-// 初始化结束
-func EndOfInitialization(c Connect, coa CauseOfTransmission, ca CommonAddr,
-	ioa InfoObjAddr, coi CauseOfInitial) error {
+// 在监视方向系统信息的应用服务数据单元
+
+// EndOfInitialization send a type identification [M_EI_NA_1],初始化结束,只有单个信息对象(SQ = 0)
+// [M_EI_NA_1] See companion standard 101,subclass 7.3.3.1
+// 传送原因(coa)用于
+// 监视方向：
+// <4> := 被初始化
+func EndOfInitialization(c Connect, coa CauseOfTransmission, ca CommonAddr, ioa InfoObjAddr, coi CauseOfInitial) error {
 	if err := c.Params().Valid(); err != nil {
 		return err
 	}
@@ -24,7 +28,7 @@ func EndOfInitialization(c Connect, coa CauseOfTransmission, ca CommonAddr,
 	return c.Send(u)
 }
 
-// GetEndOfInitialization get GetEndOfInitialization for asud when the identification M_EI_NA_1
+// GetEndOfInitialization get GetEndOfInitialization for asud when the identification [M_EI_NA_1]
 func (this *ASDU) GetEndOfInitialization() (InfoObjAddr, CauseOfInitial) {
 	return this.DecodeInfoObjAddr(), ParseCauseOfInitial(this.infoObj[0])
 }
