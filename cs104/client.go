000a36de72f7efab6d2e353b0e427422892b802d
mo <thinkgo@aliyun.com>
2019-11-12 15:55:11 +0800

    fix

--- cs104/client.go
+++ cs104/client.go
@@ -238,14 +238,12 @@ func (sf *Client) recvLoop() {
 				}
 			} else {
 				if rawData[0] != startFrame {
-					rdCnt = 0
-					length = 2
+					rdCnt, length = 0, 2
 					continue
 				}
 				length = int(rawData[1]) + 2
 				if length < APCICtlFiledSize+2 || length > APDUSizeMax {
-					rdCnt = 0
-					length = 2
+					rdCnt, length = 0, 2
 					continue
 				}
 				if rdCnt == length {
